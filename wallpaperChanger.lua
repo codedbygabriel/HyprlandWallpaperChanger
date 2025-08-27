46f7c338ab8ab1cb4a3e364a38fc82a5807167bf
João Gabriel <contato.joaogabriel@proton.me>
2025-08-27 14:27:48 -0300

    Init: First commit, already some code and functionalities.

---
+++ wallpaperChanger.lua
@@ -0,0 +1,57 @@
+local lfs = require("lfs")
+local path = string.format("/home/%s/Wallpapers", os.getenv("USER"))
+
+local wallpapersList = {}
+
+local function getLoadedWallpapers()
+	-- uses the output of hyprctl do define the initial table
+	local cmd = "hyprctl hyprpaper listloaded"
+	local res = io.popen(cmd, "r")
+	for line in res:lines("l") do
+		table.insert(wallpapersList, line)
+	end
+end
+
+local function isInTheTable(wallpaperTable, value)
+	-- Lua does not have a freaking not in operator :P
+	for key, valueTable in pairs(wallpaperTable) do
+		if valueTable == value then
+			return true
+		end
+	end
+	return false
+end
+
+local function loadWallpapers(wallpaperTable)
+	-- Final step, loads wallpaper to memory
+	for value, wallpaper in pairs(wallpaperTable) do
+		local cmd = string.format("hyprctl hyprpaper preload %s", wallpaper)
+		local res = os.execute(cmd)
+	end
+end
+
+local function getWallpapersFromDir()
+	-- gets wallpaper from dir and compares them to the already loaded (avoids duplicates)
+	for fileName in lfs.dir(path) do
+		if fileName ~= "." and fileName ~= ".." then
+			local newPath = path .. "/" .. fileName
+			if isInTheTable(wallpapersList, newPath) == false then
+				local cmd = string.format("echo 'preloading file... %s'", fileName)
+				os.execute(cmd)
+				table.insert(wallpapersList, newPath)
+			end
+		end
+	end
+end
+
+local function randomizeWallpaper(wallpaperTable)
+	local index = math.random(1, #wallpaperTable)
+	local selectedWallpaper = wallpaperTable[index]
+	local cmd = string.format('hyprctl hyprpaper wallpaper "eDP-1,%s"', selectedWallpaper)
+	os.execute(cmd)
+end
+
+getLoadedWallpapers()
+getWallpapersFromDir()
+loadWallpapers(wallpapersList)
+randomizeWallpaper(wallpapersList)
